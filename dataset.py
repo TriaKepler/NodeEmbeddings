f35c4aeeb284a62462370cb0f67b304aa6751bce
TriaKepler <92771225+TriaKepler@users.noreply.github.com>
2021-10-23 02:03:54 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 
 class GraphDataset(Dataset):
-    def __init__(self, graph, size, labels):
+    def __init__(self, graph, size, labels, misc=False):
         self.nodes_num = len(graph.nodes)
         self.graph = graph
         self.edges = [[j[0], j[1]] for j in self.graph.edges]
@@ -14,6 +14,7 @@ class GraphDataset(Dataset):
         self.data = np.zeros((len(self), 4)).astype(np.int64)
         self.main_labels = labels
         self.labels = np.zeros((len(self), 4)).astype(np.int64)
+        self.misc = misc
         self.prepare()
 
     def __len__(self):
@@ -26,8 +27,12 @@ class GraphDataset(Dataset):
         for s in range(self.size):
             rnd.shuffle(self.edges)
             for i, edge in enumerate(tqdm(self.edges)):
-                n1, n2 = edge
-                n3, n4 = rnd_choice(self.nodes_num, 2, replace=False)
+                if self.misc:
+                    if rnd.random() < 0.7:
+                        n1, n2 = edge
+                        n3, n4 = rnd_choice(self.nodes_num, 2, replace=False)
+                    else:
+                        n1, n2, n3, n4 = rnd_choice(self.nodes_num, 4, replace=False)
                 for idx, node in enumerate([n1, n2, n3, n4]):
                     self.data[i + s * self.len_edges][idx] = node
                     self.labels[i + s * self.len_edges][idx] = self.main_labels[node]
